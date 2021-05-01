863ed55a94d1bade277100a222d8b8fa852767c9
sercan navruzcan <81415090+sercannavruzcan@users.noreply.github.com>
2021-05-01 12:11:42 +0300

    OOpIntroOdev

---
+++ src/classesWithAttribute/ProductMannager.java
@@ -0,0 +1,14 @@
+package classesWithAttribute;
+
+public class ProductMannager {
+	public void Add(Product product) {
+		//JDBC
+		System.out.println("Ürün eklendi " + product.get_name());
+		
+	}
+	
+	public void Add2(int id, String name, String description, int stockAmount, double price, String renk) {
+		
+	}
+
+}
